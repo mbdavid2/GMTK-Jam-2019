85192e49f51453b718d20de2105579ca740757b3
jacknick7 <josep.ciurana@est.fib.upc.edu>
2019-08-03 13:35:53 +0200

    main class sense + sense enum

---
+++ GMTK-GameJam-2019-Unity/Assets/Code/SenseBehaviour.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SenseBehaviour : MonoBehaviour {
+
+    public enum Sense {Sight, Hearing, Touch};
+    Sense currentSense;
+
+    float currentCooldown;
+    [SerializeField] float cooldown;
+
+    void Start () {
+        setCurrentSense(Sense.Sight);
+        currentCooldown = 0;
+        updateEnvironmentSense();
+    }
+
+    void updateEnvironmentSense() {
+        switch(currentSense) {
+            case Sense.Sight:
+                break;
+            case Sense.Hearing:
+                break;
+            case Sense.Touch:
+                break;
+            default:
+                Debug.Log("wrong currentSense value");
+                break;
+        }
+    }
+
+    public Sense getCurrentSense()  {
+        return currentSense;
+    }
+
+    public void setCurrentSense(Sense sense) {
+        currentSense = sense;
+        currentCooldown = cooldown;
+        updateEnvironmentSense();
+    }
+
+    public float getCurrentCooldown() {
+        return currentCooldown;
+    }
+
+    void Update () {
+        if (currentCooldown - Time.deltaTime > 0) {
+            currentCooldown -= Time.deltaTime;
+        }
+        else if (currentCooldown != 0) {
+            currentCooldown = 0;
+        }
+    }
+}
