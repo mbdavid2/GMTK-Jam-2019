76f627a8d469668114dd350685f7a52e123ea060
jacknick7 <josep.ciurana@est.fib.upc.edu>
2019-08-04 01:27:02 +0200

    menu v2.0

--- GMTK-GameJam-2019-Unity/Assets/Code/Menu.cs
+++ GMTK-GameJam-2019-Unity/Assets/Code/Menu.cs
@@ -6,12 +6,15 @@ using UnityEngine.UI;
 
 public class Menu : MonoBehaviour {
 
-	public Button playButton;
-	public Button exitButton;
+	[SerializeField] Button playButton, creditsButton, exitButton, backButton;
+    [SerializeField] GameObject title1, title2, copy1, copy2;
 	
 	void Start () {
 		playButton.onClick.AddListener(loadGame);
+        creditsButton.onClick.AddListener(showCredits);
 		exitButton.onClick.AddListener(exitGame);
+        backButton.onClick.AddListener(showMenu);
+        showMenu();
 	}
 	
 	void FixedUpdate () {
@@ -20,11 +23,34 @@ public class Menu : MonoBehaviour {
 		}
 	}
 
-	private void loadGame() {
+	void loadGame() {
 		SceneManager.LoadScene("Main");
 	}
 
-	private void exitGame() {
+	void exitGame() {
 		Application.Quit();
 	}
+
+    void showCredits() {
+        title1.SetActive(false);
+        title2.SetActive(true);
+        copy1.SetActive(true);
+        copy2.SetActive(true);
+        playButton.gameObject.SetActive(false);
+        creditsButton.gameObject.SetActive(false);
+        exitButton.gameObject.SetActive(false);
+        backButton.gameObject.SetActive(true);
+
+    }
+
+    void showMenu() {
+        title1.SetActive(true);
+        title2.SetActive(false);
+        copy1.SetActive(false);
+        copy2.SetActive(false);
+        playButton.gameObject.SetActive(true);
+        creditsButton.gameObject.SetActive(true);
+        exitButton.gameObject.SetActive(true);
+        backButton.gameObject.SetActive(false);
+    }
 }
