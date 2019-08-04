c73734393b77634828758495a0e50f3f47f51f1a
jacknick7 <josep.ciurana@est.fib.upc.edu>
2019-08-04 19:58:53 +0200

    GameOver final version?

--- GMTK-GameJam-2019-Unity/Assets/Code/GameOver.cs
+++ GMTK-GameJam-2019-Unity/Assets/Code/GameOver.cs
@@ -8,14 +8,27 @@ public class GameOver : MonoBehaviour {
 
     [SerializeField] Button nextLevelButton, menuButton, exitButton;
     [SerializeField] Text title, text;
+    [SerializeField] AudioSource win, lose;
 
-    // Use this for initialization
     void Start () {
+        string[] quotes = new string[10];
+        quotes[0] = "\"The world is full of obvious things which nobody by any chance ever observes.\"\n-Arthur Conan Doyle";
+        quotes[1] = "\"The detective and his criminal wear versions of the same mask.\"\n-Jane Roberts";
+        quotes[2] = "\"A detective sees death in all the various forms at least five times a week.\"\n-Evan Hunter";
+        quotes[3] = "\"The instant they say something you don't expect or already know, you should drop into detective mode.\"\n-Emmett Shear";
+        quotes[4] = "\"To read of a detective's daring finesse or ingenious stratagem is a rare joy.\"\n-Rex Stout";
+        quotes[5] = "\"The main purpose of a significance test is to inhibit the natural enthusiasm of the investigator.\"\n-Frederick Mosteller";
+        quotes[6] = "\"Read like a detective and write like a conscientious investigative reporter.\"\n-David Coleman";
+        quotes[7] = "\"The criminal is the creative artist; the detective only the critic.\"\n-Gilbert K. Chesterton";
+        quotes[8] = "\"The detective story is the normal recreation of noble minds.\"\n-Philip Guedalla";
+        quotes[9] = "\"It is ridiculous to set a detective story in New York City. New York City is itself a detective story.\"\n-Agatha Christie";
         if (Menu.gameover) {
+            lose.Play();
             nextLevelButton.gameObject.SetActive(false);
-            text.text = "You lose, better luck next time.";
+            text.text = "You lose, better luck next time.\n\n" + quotes[Random.Range(0,10)];
         }
         else {
+            win.Play();
             Menu.currentLevel++;
             if (Menu.currentLevel > 4) {
                 Menu.currentLevel = 0;
@@ -24,8 +37,8 @@ public class GameOver : MonoBehaviour {
                 nextLevelButton.gameObject.SetActive(false);
             }
             else {
-                title.text = "Level Clear";
-                text.text = "You found the culprit, nice.";
+                title.text = "Level Cleared";
+                text.text = "You found the culprit, nice.\n\n" + quotes[Random.Range(0, 10)];
             }
             nextLevelButton.onClick.AddListener(() => SceneManager.LoadScene(Menu.currentLevel));
         }
